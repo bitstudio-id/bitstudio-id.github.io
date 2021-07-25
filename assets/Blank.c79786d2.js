347cd0f2f3dcb72beb329c04c42e4f2d697b2f4f
cacing69 <cacing69@users.noreply.github.com>
2021-07-25 07:38:02 +0000

    Update vite-vue3-antd 2509f3a364d19acf0313b63f3424d1bee04705fd

---
+++ assets/Blank.c79786d2.js
@@ -0,0 +1,1 @@
+import{d as r,o as e,b as d}from"./vendor.b2dd1187.js";var n=r({setup(){}});n.render=function(r,n,a,o,t,u){return e(),d("div",null,"Blank.vue")};export default n;
--- assets/blank.b79aa40e.js
+++
@@ -1,1 +0,0 @@
-import{b as e,o as r,e as a}from"./vendor.e4a3479d.js";var n=e({setup(){}});n.render=function(e,n,o,t,u,d){return r(),a("div",null,"Blank.vue")};export default n;
