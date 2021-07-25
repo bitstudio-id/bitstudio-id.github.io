f5094b322cc714c637388b0a0b7df7f172d72de4
cacing69 <cacingworm69@gmail.com>
2021-07-25 10:16:06 +0700

    fix(lazy-load): enable lazy-load use defineAsyncComponent

---
+++ src/modules/blank/views/blank.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Blank.vue</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,22 @@
-/* eslint-disable import/no-unresolved */
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-// import Home from "@/modules/home/views/home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: import('@/modules/home/views/home.vue')
+    component: defineAsyncComponent(() => import('@/modules/home/views/home.vue'))
   },
   {
     path: '/login',
     name: 'login',
-    component: import('@/modules/login/views/login.vue')
+    component: defineAsyncComponent(() => import('@/modules/login/views/login.vue'))
+  },
+  {
+    path: '/blank',
+    name: 'blank',
+    component: defineAsyncComponent(() => import('@/modules/blank/views/blank.vue'))
   }
-  // {
-  //     path: "/vuex",
-  //     name: "Vuex",
-  //     component: Vuex,
-  // },
-  // {
-  //     path: "/axios",
-  //     name: "Axios",
-  //     component: () => import("@/views/Axios.vue"), // lazy-load
-  // },
 ]
 const router = createRouter({
   history: createWebHistory(),
