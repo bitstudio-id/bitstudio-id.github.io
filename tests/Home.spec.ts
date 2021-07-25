21944adec7dbb4e5c11a7d5dfa976ae48b73ef73
cacing69 <cacingworm69@gmail.com>
2021-07-25 14:34:04 +0700

    test(home): home

--- tests/Home.spec.ts
+++ tests/Home.spec.ts
@@ -3,7 +3,6 @@ import Home from '../src/modules/home/views/home.vue'
 
 test('Home.vue', async () => {
   const wrapper = mount(Home)
-  expect(wrapper.html()).toContain('Lorem ipsum dolor sit amet')
   expect(wrapper.html()).toContain('Home.vue')
   expect(wrapper.html()).toContain('Text 0')
   await wrapper.find('button').trigger('click')
